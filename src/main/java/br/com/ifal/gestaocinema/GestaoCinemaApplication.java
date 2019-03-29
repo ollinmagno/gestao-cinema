93a2ca723f753bc9cf1f64038e176ae2647461a5
Ollin Magno <>
2019-03-29 12:56:24 -0300

    first commit

---
+++ src/main/java/br/com/ifal/gestaocinema/GestaoCinemaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifal.gestaocinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCinemaApplication.class, args);
+	}
+
+}
